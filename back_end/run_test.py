5b44a02f110d66a841f7e5444b44bf24524af8d4
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-06-24 08:24:18 +0200

    testing

--- back_end/run_test.py
+++ back_end/run_test.py
@@ -9,14 +9,15 @@ from openeo_pg_parser import graph
 from jeodpp_backend import BackEnd
 
 jeodpp=BackEnd('jeodpp')
-#graph = translate_process_graph("tests/process_graphs/zonal_statistics_test.json")
-#graph = translate_process_graph("tests/process_graphs/zonal_statistics.json")
-#graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries.json")
 graph = translate_process_graph("tests/process_graphs/evi_eodc_1.json")
 graph = translate_process_graph("tests/process_graphs/s2_max_ndvi.json")
 graph = translate_process_graph("tests/process_graphs/evi_eodc.json")
 graph = translate_process_graph("tests/process_graphs/evi_eodc_test.json")
 graph = translate_process_graph("tests/process_graphs/min_evi_jeodpp.json")
+graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries.json")
+graph = translate_process_graph("tests/process_graphs/zonal_statistics.json")
+graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries_test.json")
+graph = translate_process_graph("tests/process_graphs/zonal_statistics_test.json")
 
 
 print(graph.sort())
