526785a67b9ef1fb9ee5172a7ddc9818e7449233
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-07-07 17:02:54 +0200

    either filter on mgrs or filter_bbox

--- back_end/jeodpp_backend.py
+++ back_end/jeodpp_backend.py
@@ -108,13 +108,13 @@ class BackEnd:
             mgrs = node.content['arguments'].get('spatial_extent').get('mgrs')
             if mgrs is not None:
                 coll.filterOn('mgrs',str(mgrs))
-
-            coll.filter_bbox(west=west,
-                             east=east,
-                             north=north,
-                             south=south,
-                             regions=features,
-                             crs=crs)
+            else:
+                coll.filter_bbox(west=west,
+                                 east=east,
+                                 north=north,
+                                 south=south,
+                                 regions=features,
+                                 crs=crs)
             #filter on dates:
 
             print("temporal_extent: {}".format(node.content['arguments']['temporal_extent']))
