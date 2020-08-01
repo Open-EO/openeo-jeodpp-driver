e60bc1af354b0ca30fb6590f85bf6565d93fa61a
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-08-01 18:49:53 +0200

    multi-process aggregate_spatial not yet supported

--- back_end/run_test.py
+++ back_end/run_test.py
@@ -39,10 +39,11 @@ graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeserie
 graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries_geometry.json")
 graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries_mgrs.json")
 graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries_file.json")
+graph = translate_process_graph("tests/process_graphs/zonal_statistics_timeseries_ndvi_file.json")
 
 #print(graph)
 print(graph.sort())
 print("memory before process (in GB): {}".format(memory_usage()))
-#jeodpp.process(graph.sort())
-jeodpp.process(graph.sort(),virtual=True)
+jeodpp.process(graph.sort())
+#jeodpp.process(graph.sort(),virtual=True)
 print("memory after process (in GB): {}".format(memory_usage()))
