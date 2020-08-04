c336bbe3de4df1fc403243c428fcde7e0aace250
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-08-04 16:33:23 +0200

    changed package name to jeodpp_backend

--- back_end/setup.py
+++ back_end/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages
 
 
 setup(
-    name='openeo-jeodpp_backend',
+    name='jeodpp_backend',
     version='0.1.0',
     author_email='pieter.kempeneers@ec.europa.eu',
     url='https://jeodpp.jrc.ec.europa.eu/apps/gitlab/jeodpp/openeo/-/tree/master/back_end',
