434656dccc5ed96654d4ff82261d2381daaf5e4b
Tomas Kliment <tomas@klimeto.com>
2020-09-07 13:45:33 +0200

    add first version of fastapi package

---
+++ fastapi/webapp/__init__.py
@@ -0,0 +1,37 @@
+import logging
+
+
+from fastapi import FastAPI
+from fastapi_utils.api_settings import get_api_settings
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from .settings import get_settings
+from . import collection
+
+
+logger = logging.getLogger(__name__)
+
+def get_app() -> FastAPI:
+    # settings
+    get_api_settings.cache_clear()
+    api_settings = get_api_settings()
+    #settings = get_settings()
+    # app
+    app = FastAPI(**api_settings.fastapi_kwargs)
+    # middleware
+    app.add_middleware(
+        CORSMiddleware,
+        #allow_origins=settings.backend_cors_origins.splitlines(),
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    # routes
+    app.include_router(collection.router, prefix="/collections", tags=["collections"])
+    return app
+
+
+app = get_app()
+
---
+++ fastapi/webapp/settings.py
@@ -0,0 +1,61 @@
+import functools
+import logging
+import os
+
+from pydantic import BaseSettings
+
+logger = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    """ Parses variables from environment on instantiation """
+
+    deploy_env: str
+    backend_cors_origins: str
+
+    db_host: str
+    db_port: int
+    postgres_db: str
+    db_rpi_user: str
+    db_rpi_pass: str
+
+    testing: bool = False
+
+    @property
+    def db_uri(self):
+        uri = "postgresql://{db_rpi_user}:{db_rpi_pass}@{db_host}:{db_port}/{postgres_db}"
+        uri = uri.format(**self.__dict__)
+        return uri
+
+    postgres_user: str
+    postgres_password: str
+
+    @property
+    def db_super_uri(self) -> str:
+        uri = "postgresql://{postgres_user}:{postgres_password}@{db_host}:{db_port}/{postgres_db}"
+        uri = uri.format(**self.__dict__)
+        return uri
+
+    @property
+    def db_uri_test(self) -> str:
+        uri = "postgresql://{db_rpi_user}:{db_rpi_pass}@{db_host}:{db_port}/{postgres_db}_test"
+        uri = uri.format(**self.__dict__)
+        return uri
+
+    @property
+    def db_super_uri_test(self) -> str:
+        uri = "postgresql://{postgres_user}:{postgres_password}@{db_host}:{db_port}/{postgres_db}_test"
+        uri = uri.format(**self.__dict__)
+        return uri
+
+    @property
+    def db_super_uri_postgres(self) -> str:
+        uri = "postgresql://{postgres_user}:{postgres_password}@{db_host}:{db_port}/postgres"
+        uri = uri.format(**self.__dict__)
+        return uri
+
+
+@functools.lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
