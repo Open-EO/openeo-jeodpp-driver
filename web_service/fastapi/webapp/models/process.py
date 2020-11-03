7623b0a08d3e6a73370734bd75f86c23a4b14439
Tomas Kliment <tomas@klimeto.com>
2020-11-03 12:52:15 +0100

    fastapi: add process DB table definition + alembic migration

--- web_service/fastapi/webapp/models/process.py
+++ web_service/fastapi/webapp/models/process.py
@@ -1,10 +1,58 @@
 from typing import Any
 
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+
 from .base import PydanticBase
+from .base import Base
+from .base import TimeStampMixin
 
 __all__ = ["ProcessGraph"]
 
 
+#SQLAlchemy models
+class Process(Base, TimeStampMixin):
+    __tablename__ = "process"
+    id = sa.Column(sa.String, nullable=False, unique=True, primary_key=True)
+    summary = sa.Column(sa.String, nullable=True)
+    description = sa.Column(sa.String, nullable=False)
+    categories = sa.Column(JSONB, nullable=True)
+    parameters = sa.Column(JSONB, nullable=False)
+    returns = sa.Column(JSONB, nullable=False)
+    deprecated = sa.Column(sa.Boolean, default=False)
+    experimental = sa.Column(sa.Boolean, default=False)
+    exceptions = sa.Column(JSONB, nullable=True)
+    examples = sa.Column(JSONB, nullable=True)
+    links = sa.Column(JSONB, nullable=True)
+    process_graph = sa.Column(JSONB, nullable=True)
+    
+
+    __table_args__ = (
+        sa.PrimaryKeyConstraint(
+            "id",
+        ),
+        sa.Index(
+            "idx_process_identifier", "id", postgresql_using="btree"
+        ),
+        sa.Index(
+            "idx_process_description", "description", postgresql_using="btree"
+        ),
+        sa.Index(
+            "idx_process_parameters", "parameters", postgresql_using="gin"
+        ),
+        sa.Index(
+            "idx_process_categories", "categories", postgresql_using="gin"
+        ),
+        sa.Index(
+            "idx_process_returns", "returns", postgresql_using="gin"
+        ),
+    )
+
+
+
+#Pydantic models
 class ProcessGraph(PydanticBase):
     process_id: str
     process_description: str
