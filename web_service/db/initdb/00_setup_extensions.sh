3921892a8fc1814fce0b67682969d4a659ef76c6
Tomas Kliment <tomas@klimeto.com>
2020-11-10 14:34:22 +0100

    db: add extension to generate uuid4

--- web_service/db/initdb/00_setup_extensions.sh
+++ web_service/db/initdb/00_setup_extensions.sh
@@ -4,5 +4,5 @@
 set -xeuo pipefail
 
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
-    CREATE EXTENSION pgcrypto;
+    CREATE EXTENSION pgcrypto; CREATE EXTENSION "uuid-ossp";
 EOSQL
