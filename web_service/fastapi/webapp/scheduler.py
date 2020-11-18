8fce75444b9ead2cdf4becddb4fb20ed03d12bf5
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-11-18 11:39:32 +0100

    updated scheduler.py with todo's

--- web_service/fastapi/webapp/scheduler.py
+++ web_service/fastapi/webapp/scheduler.py
@@ -1,10 +1,12 @@
 
 def job_queue(job_id: str):
-    job_estimate = {
+    #todo: queue job in kubernetes
+    #todo: update dictionary with meaningful values
+    job_queue = {
         "costs": 12.98,
         "duration": "P1Y2M10DT2H30M",
         "size": 157286400,
         "downloads_included": 5,
         "expires": "2020-11-01T00:00:00Z"
     }
-    return job_estimate
+    return job_queue
