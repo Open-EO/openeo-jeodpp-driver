6201779aae22489135654c207df91482c2260569
Tomas Kliment <tomas@klimeto.com>
2020-11-18 15:52:20 +0100

    fastapi: remove htcondor and scheduler modules

--- web_service/fastapi/webapp/htcondor.py
+++
@@ -1,9 +0,0 @@
-def job_estimate(job_id: str):
-    job_estimate = {
-        "costs": 12.98,
-        "duration": "P1Y2M10DT2H30M",
-        "size": 157286400,
-        "downloads_included": 5,
-        "expires": "2020-11-01T00:00:00Z"
-    }
-    return job_estimate
